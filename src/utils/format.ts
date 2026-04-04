5a19c83766cb487c921ee06f4d0b822c31a54d96
tsrajpurohit <156318103+tsrajpurohit@users.noreply.github.com>
2026-04-04 16:10:33 +0530

    Update format.ts

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,5 +1,5 @@
 export const formatCurrency = (amount: number): string => {
-  return new Intl.NumberFormat('en-US', {
+  return new Intl.NumberFormat('en-IN', {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   }).format(amount);
