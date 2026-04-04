90b32efdf2383a9fa0ff364a4624dc078d3f6d9f
tsrajpurohit <156318103+tsrajpurohit@users.noreply.github.com>
2026-04-04 14:39:59 +0530

    Add files via upload

---
+++ src/utils/format.ts
@@ -0,0 +1,6 @@
+export const formatCurrency = (amount: number): string => {
+  return new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(amount);
+};
